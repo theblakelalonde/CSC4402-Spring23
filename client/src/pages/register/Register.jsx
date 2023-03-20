01676478d0ee564ae8ebe3e3e5fc94ed11392336
Blake Lalonde <balalonde01@gmail.com>
2023-03-20 14:59:31 -0500

    Changed some things

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./register.scss"
+import "./register.scss";
 import axios from "axios";
 
 const Register = () => {
-
   const [inputs, setInputs] = useState({
     username: "",
     email: "",
@@ -13,53 +12,75 @@ const Register = () => {
     password: "",
   });
 
-
   const [err, setErr] = useState(null);
-
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handleClick = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       await axios.post("http://localhost:8800/backend/auth/register", inputs);
     } catch (err) {
       setErr(err.response.data);
     }
-  }
+  };
 
-  console.log(err)
-
+  console.log(err);
 
   return (
     <div className="register">
       <div className="card">
         <div className="left">
           {/*//TODO change h1 and p */}
-          <h1>MOG World</h1>
+          <h1>MOG</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
             consequatur.
           </p>
-          <span>ALready have an account?</span>
+          <span>Already have an account?</span>
           <Link to="/login">
             <button>Login</button>
           </Link>
         </div>
         <div className="right">
-          <h1>Login</h1>
-          <form >
-            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
-            <input type="email" placeholder="Email" name="email" onChange={handleChange} />
-            <input type="text" placeholder="First Name" name="firstName" onChange={handleChange} />
-            <input type="text" placeholder="Last Name" name="lastName" onChange={handleChange} />
-            <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+          <h1>Register</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
             {err && err}
-            <button onClick={handleClick}>Register</button>
+            <button onClick={handleClick}>Create Account</button>
           </form>
         </div>
       </div>
