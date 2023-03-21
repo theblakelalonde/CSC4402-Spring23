a9f45ba14a9ed7ca9d71f5c36058da5140f6c423
Blake Lalonde <balalonde01@gmail.com>
2023-03-21 17:43:36 -0500

    changed comment button

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,48 +1,49 @@
-import "./comments.scss"
+import "./comments.scss";
 import { useContext } from "react";
-import {AuthContext} from "../../context/authContext"
+import { AuthContext } from "../../context/authContext";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 
 const Comments = () => {
+  const { currentUser } = useContext(AuthContext);
 
-    const{currentUser} = useContext(AuthContext)
+  const comments = [
+    {
+      id: 1,
+      desc: "Dude massive cannon arms you got there! :O",
+      name: "John Cena",
+      userId: 1,
+      profilePic:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
+    },
+    {
+      id: 2,
+      desc: "Wow! 😘",
+      name: "John Goober",
+      userId: 2,
+      profilePic:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
+    },
+  ];
+  return (
+    <div className="comments">
+      <div className="write">
+        <img src={currentUser.profilePic} alt="" />
+        <input type="text" placeholder="Write a comment" />
+        <SendRoundedIcon className="sendButton" />
+        {/* <button>Send</button> */}
+      </div>
+      {comments.map((comment) => (
+        <div className="comment">
+          <img src={comment.profilePic} alt="" />
+          <div className="info">
+            <span>{comment.name}</span>
+            <p>{comment.desc}</p>
+          </div>
+          <span className="date">1 hour ago</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    const comments = [
-        {
-          id: 1,
-          desc: "Dude massive cannon arms you got there! :O",
-          name: "John Cena",
-          userId: 1,
-          profilePic:
-            "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
-        },
-        {
-          id: 2,
-          desc: "Wow! 😘",
-          name: "John Goober",
-          userId: 2,
-          profilePic:
-            "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
-        },
-      ];
-    return (
-        <div className="comments">
-            <div className="write">
-            <img src={currentUser.profilePic} alt=""/>
-            <input type="text" placeholder="Write a comment" />
-            <button>Send</button>
-            </div>
-            {comments.map(comment=>(
-                <div className="comment">
-                    <img src={comment.profilePic} alt="" />
-                    <div className="info">
-                        <span>{comment.name}</span>
-                        <p>{comment.desc}</p>
-                    </div>
-                    <span className="date">1 hour ago</span>
-                </div>
-            ))
-            }</div>
-    )
-}
-
-export default Comments
+export default Comments;
