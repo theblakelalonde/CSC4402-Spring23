9478886e99dcf2b043b533faa1d0ece423308489
Bryan Tran <bryantran21@gmail.com>
2023-03-21 20:20:43 -0500

    hard coded profile segment implemented

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,44 @@
-import "./profile.scss"
+import "./profile.scss";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import Posts from "../../components/posts/Posts";
 
-const profile = () => {
+const Profile = () => {
   return (
-    <div className="profile">profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>Baton Rouge</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Mog</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
+};
 
-export default profile
+export default Profile;
