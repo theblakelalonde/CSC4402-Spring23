f892b2e8f8c2d8e2bc26de43ba3053d9357eb143
Horacio Medina <61767007+hmedin@users.noreply.github.com>
2023-03-22 01:10:04 -0500

    posting works for users

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getPosts, addPost } from "../controllers/post.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", getPosts)
-router.get("/", addPost)
+router.get("/", getPosts);
+router.post("/", addPost);
 
-export default router
+export default router;
