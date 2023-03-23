9906a763fd8cae6b4ee67461b5fd3101e30fa471
Blake Lalonde <balalonde01@gmail.com>
2023-03-23 16:47:12 -0500

    responsiveness + comment and post reformat

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -2,53 +2,45 @@ import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
-
 export const getPosts = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not logged in!")
-    jwt.verify(token, "secretkey", (err, userInfo) => {
-        if (err) return res.status(403).json("Token is not valid!");
-
-
-        // Filters posts to only see posts from people you followed and yourself
-        const q = `SELECT p.*, u.userID as uID, firstName, lastName, profilePic FROM posts AS p 
+    // Filters posts to only see posts from people you followed and yourself
+    const q = `SELECT p.*, u.userID as uID, userName, firstName, lastName, profilePic FROM posts AS p 
         JOIN users AS u ON (u.userID = p.userID)
         LEFT JOIN relationships AS r ON (p.userID = r.followedUserID) WHERE r.followerUserID= ? OR p.userID =?
         ORDER BY p.createdAt DESC`;
 
-
-        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json(data);
-
-        });
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
     });
+  });
 };
 
 export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not logged in!")
-    jwt.verify(token, "secretkey", (err, userInfo) => {
-        if (err) return res.status(403).json("Token is not valid!");
+    // Filters posts to only see posts from people you followed and yourself
+    const q =
+      "INSERT INTO posts (`desc`, `img`, `createdAt`, `userID`) VALUES (?)";
 
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
 
-        // Filters posts to only see posts from people you followed and yourself
-        const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userID`) VALUES (?)";
-
-        const values = [
-            req.body.desc,
-            req.body.img,
-            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-            userInfo.id
-        ]
-
-
-        db.query(q, [values], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("Post has been created.");
-
-        });
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created.");
     });
+  });
 };
