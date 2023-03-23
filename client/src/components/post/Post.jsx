1c8cf7285078caed60c76f6b240835fc7eb8d7e3
Anthony Duong <anthonyduong17@gmail.com>
2023-03-22 22:59:45 -0500

    fixing post error

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -9,47 +9,51 @@ import Comments from "../comments/Comments";
 import { useState } from "react";
 import moment from "moment";
 
-const Post = ({post}) => {
-    //TEMPORARY
-    const liked = false;
-    const [commentOpen, setCommentOpen] = useState(false)
-    return (
-        <div className='post'>
-            <div className="container">
-                <div className='user'>
-                    <div className="userInfo">
-                        <img src={post.profilePic} alt="" />
-                        <div className="details">
-                            <Link to={`/profile/${post.userID}`} style={{textDecoration:"none", color:"inherit"}}>
-                                <span className="name">{post.firstName + ' ' + post.lastName}</span>
-                            </Link>
-                            <span className="date">1 min ago</span>
-                        </div>
-                    </div>
-                    <MoreHorizIcon/>
-                </div>
-                <div className='content'>
-                    <p>{post.desc}</p>
-                    <img src={post.img} alt="" />
-                </div>
-                <div className='info'>
-                    <div className="item">
-                        {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-                        69 Likes
-                    </div>
-                    <div className="item" onClick={()=>setCommentOpen(!commentOpen)}>
-                        <TextsmsOutlinedIcon />
-                        3 Comments
-                    </div>
-                    <div className="item">
-                        <ShareOutlinedIcon />
-                        10 Shares
-                    </div>
-                </div>
-                {commentOpen && <Comments/>}
+const Post = ({ post }) => {
+  //TEMPORARY
+  const liked = false;
+  const [commentOpen, setCommentOpen] = useState(false);
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="" />
+            <div className="details">
+              <Link
+                to={`/profile/${post.userID}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <span className="name">
+                  {post.firstName + " " + post.lastName}
+                </span>
+              </Link>
+              <span className="date">{moment(post.createdAt).fromNow()}</span>
             </div>
+          </div>
+          <MoreHorizIcon />
         </div>
-    )
-}
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={"./upload/" + post.img} alt="" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            69 Likes
+          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon />3 Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            10 Shares
+          </div>
+        </div>
+        {commentOpen && <Comments />}
+      </div>
+    </div>
+  );
+};
 
 export default Post;
