94d3528f23e4f43ed847574325f027c5ce101e0f
Horacio Medina <61767007+hmedin@users.noreply.github.com>
2023-03-22 21:35:48 -0500

    photo posting

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 //middlewares
 app.use((req, res, next) => {
@@ -22,6 +23,23 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/backend/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
 app.use(cookieParser());
 
 app.use("/backend/auth", authRoutes);
--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -3,53 +3,74 @@ import Image from "../../assets/img.png";
 import Friend from "../../assets/friend.png";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
-
 const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
 
-  const [file,setFile] = useState(null)
-  const [desc,setDesc] = useState("")
-
-  const {currentUser} = useContext(AuthContext)
-
-  const queryClient = useQueryClient()
-
-  const mutation = useMutation(
-    (newPost)=>{
-    return makeRequest.post("/posts", newPost)
-  }, 
-  {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["posts"])
-    },
-  }
-  );
-
-
-  const handleClick = (e) =>{
-    e.preventDefault()
-    mutation.mutate({desc})
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
+  const { currentUser } = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let imgURL = "";
+    if (file) imgURL = await upload();
+    mutation.mutate({ desc, img: imgURL });
+    setDesc("");
+    setFile(null);
+  };
 
   return (
     <div className="share">
       <div className="container">
         <div className="top">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <input type="text" placeholder={`What's on your mind ${currentUser.firstName}?`} 
-          onChange={(e)=>setDesc(e.target.value)}/>
+          <div className="left">
+            <img src={currentUser.profilePic} alt="" />
+            <input
+              type="text"
+              placeholder={`What's on your mind ${currentUser.firstName}?`}
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="right">
+            {file && <img className="file" src={URL.createObjectURL(file)} />}
+          </div>
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}}
-            onChange={(e)=>setFile(e.target.files[0])} />
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -66,7 +87,6 @@ const Share = () => {
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
