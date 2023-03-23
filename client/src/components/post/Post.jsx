c935775a5b53c5ff488cbe2f791fb95ed54eafe8
Anthony Duong <anthonyduong17@gmail.com>
2023-03-22 20:37:40 -0500

    Fixed Post.jsx

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,4 +1,4 @@
-import "./post.scss";
+import "./post.scss"
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
@@ -9,51 +9,47 @@ import Comments from "../comments/Comments";
 import { useState } from "react";
 import moment from "moment";
 
-const Post = ({ post }) => {
-  //TEMPORARY
-  const liked = false;
-  const [commentOpen, setCommentOpen] = useState(false);
-  return (
-    <div className="post">
-      <div className="container">
-        <div className="user">
-          <div className="userInfo">
-            <img src={post.profilePic} alt="" />
-            <div className="details">
-              <Link
-                to={`/profile/${post.userID}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                <span className="name">
-                  {post.firstName + " " + post.lastName}
-                </span>
-              </Link>
-              <span className="date">{moment(post.createdAt).fromNow()}</span>
+const Post = ({post}) => {
+    //TEMPORARY
+    const liked = false;
+    const [commentOpen, setCommentOpen] = useState(false)
+    return (
+        <div className='post'>
+            <div className="container">
+                <div className='user'>
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link to={`/profile/${post.userID}`} style={{textDecoration:"none", color:"inherit"}}>
+                                <span className="name">{post.firstName + ' ' + post.lastName}</span>
+                            </Link>
+                            <span className="date">1 min ago</span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon/>
+                </div>
+                <div className='content'>
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className='info'>
+                    <div className="item">
+                        {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+                        69 Likes
+                    </div>
+                    <div className="item" onClick={()=>setCommentOpen(!commentOpen)}>
+                        <TextsmsOutlinedIcon />
+                        3 Comments
+                    </div>
+                    <div className="item">
+                        <ShareOutlinedIcon />
+                        10 Shares
+                    </div>
+                </div>
+                {commentOpen && <Comments/>}
             </div>
-          </div>
-          <MoreHorizIcon />
         </div>
-        <div className="content">
-          <p>{post.desc}</p>
-          <img src={"./upload/" + post.img} alt="" />
-        </div>
-        <div className="info">
-          <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            69 Likes
-          </div>
-          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-            <TextsmsOutlinedIcon />3 Comments
-          </div>
-          <div className="item">
-            <ShareOutlinedIcon />
-            10 Shares
-          </div>
-        </div>
-        {commentOpen && <Comments />}
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default Post;
